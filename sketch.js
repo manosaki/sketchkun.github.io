851d1e7461cd4a830c847ef96acf2479fa4cb526
manosaki <40316699+manosaki@users.noreply.github.com>
2021-02-16 13:29:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-/* ===
-ml5 Example
-Pix2pix Edges2Pikachu example with p5.js using promises
-This uses a pre-trained model on Pikachu images
-For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/pix2pix
-=== */
-
-// The pre-trained Edges2Pikachu model is trained on 256x256 images
-// So the input images can only be 256x256 or 512x512, or multiple of 256
 const SIZE = 512;
 let inputImg, inputCanvas, outputContainer, statusMsg, transferBtn, clearBtn;
 let sX = [];
